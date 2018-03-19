df953728a9f4c360a82a15cd6d372d0b69841e79
Joshua Schnall <josh@Joshuas-MacBook-Air.local>
2018-03-18 18:11:51 -0700

    final Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.io.Console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -8,13 +7,29 @@ import java.util.Random;
 
 /*
 To do
--check and make sure exceptions work okay and clean it all up
--NOTE the multiThreading input for two players only works from the command line input!!!!!!!!!!!!!
+-test it more
+-clean it up and done...................
+*/
 
+
+/*
+-NOTE the multiThreading input for two players only works from the command line input!!!!!!!!!!!!!
+-to run in command line:  javac src/com/osama/GHide/*.java
+java -cp ./src com.osama.GHide.EnteringPoint
+
+ */
+
+/*
+simple command line java rocks paper scissors game
+works best through the command prompt for the password logging feature of the game
+can be played with one player and a computer or two players
+launches from main method, follow given instructions............
  */
 
 public class Main {
 
+    //game runs from main
+    //
     public static void main(String[] args) {
 
         //vars for possible game combos, game history etc
@@ -36,7 +51,6 @@ public class Main {
                 printIntro();
                 gameMode = s.next();
                 playerType = gameMode;
-
             }
 
         }catch(Exception error){
@@ -50,23 +64,25 @@ public class Main {
                 play(combos, history, playerType);
             } else if (gameMode.equals("history")) {
                 printHistory(history);
-
             } else {
                 quit =true;
             }
-
             try {
                 gameMode = s.next();
+                playerType = gameMode;
+                while(!(gameMode.equals("play_human") || gameMode.equals("play_computer") || gameMode.equals("history") || gameMode.equals("quit"))){
+                    printIntro();
+                    gameMode = s.next();
+                    playerType = gameMode;
+                }
                 if (gameMode.equals("quit")) {
                     quit = true;
+                    s.close();  //added here
                 }
             }catch(Exception error){
                 error.printStackTrace();
             }
         }
-
-
-
     }
 
     //create hashmap of possible game combos
@@ -91,6 +107,8 @@ public class Main {
     //print intro
     //
     private static void printIntro(){
+        System.out.println();
+        System.out.println();
         System.out.println("Welcome to Rock Paper Scissors!");
         System.out.println("MAIN MENU");
         System.out.println("=====");
@@ -103,13 +121,10 @@ public class Main {
     //play the game either in 1 player or two player mode
     //
     private static void play(HashMap<String, String> combos, ArrayList<String> history, String playerType){
-
-
         //get player vs computer or human
         Scanner s = new Scanner(System.in);
 
         if(playerType.equals("play_computer")){
-
             //create player class instances
             Player player1 = new Player(true,1);
             Player player2 = new Player(false, 2);
@@ -121,8 +136,10 @@ public class Main {
 
             //error check input
             try {
+                //computer adversary
+                //
                 answer1 = s.next();
-                answer1.toLowerCase();
+                answer1 = answer1.toLowerCase();
 
                 while(!(answer1.toLowerCase().equals("rock") || answer1.toLowerCase().equals("paper") || answer1.toLowerCase().equals("history") ||
                         answer1.equals("quit"))){
@@ -135,40 +152,10 @@ public class Main {
                     return;
                 }
 
-
                 //computer random input
-                //Random rand = new Random();
-                //int randNum = rand.nextInt(2);
-                //String[] answerRay = new String[]{"rock", "paper", "Scissors"};
-                //String answer2 = answerRay[randNum];
                 String answer2 = computerMove();
 
-
-                /*
-                //translate moves to hashmap result
-                String hashmapKey = answer1 + " " +  answer2;
-                String hashmapResult = combos.get(hashmapKey);
-                String player2Result = "";
-
-
-                //set player class attributes from this game for player1 and player2
-                if(hashmapResult.equals("win")){
-                    player2Result = "lose";
-                    player1.setPoints(player1.getPoints() +1);
-                }else{
-                    player2Result = "win";
-                    player2.setPoints(player2.getPoints() +1);
-                }
-
-                player1.setMove(answer1);
-                player1.setwOrL(hashmapResult);
-
-
-                player2.setMove(answer2);
-                player2.setwOrL(player2Result);
-                */
                 traslateToHashandResults(answer1, answer2, combos, player1, player2);
-
 
                 //print results and add to history
                 System.out.println("Computer picks: " + answer2);
@@ -177,13 +164,14 @@ public class Main {
                 String winOrLose = player1.getwOrL();
                 System.out.println("you " + winOrLose +"!");
 
-
                 addToHistory(history, player1, player2);
             }catch(Exception error){
                 error.printStackTrace();
             }
 
         }else{
+            //human adversary, not computer
+            //
 
             //create player class instances
             Player player1 = new Player(true,1);
@@ -201,13 +189,12 @@ public class Main {
                 //player1 input
                 PasswordField getMove = new PasswordField();
                 answer1 = getMove.readPassword("enter    ");
-                answer1.toLowerCase();
+                answer1= answer1.toLowerCase();
 
                 //error checking
                 while(!(answer1.toLowerCase().equals("rock") || answer1.toLowerCase().equals("paper") || answer1.toLowerCase().equals("history") ||
                         answer1.equals("quit"))){
 
-                    System.out.println("debug" + "in while");
                     printAskForMove();
                     answer1 = s.next();
                 }
@@ -227,7 +214,7 @@ public class Main {
                     //player2 input
                     getMove = new PasswordField();
                     answer2 = getMove.readPassword("enter    ");
-                    answer2.toLowerCase();
+                    answer2 = answer2.toLowerCase();
 
                     //error checking input
                     while(!(answer2.toLowerCase().equals("rock") || answer2.toLowerCase().equals("paper") || answer2.toLowerCase().equals("history") ||
@@ -240,36 +227,11 @@ public class Main {
                         printIntro();
                         return;
                     }
-
-
                 }catch(Exception error){
                     error.printStackTrace();
 
                 }
 
-
-                /*
-                //translate input to hashmap result
-                String hashmapKey = answer1 + " " +  answer2;
-                String hashmapResult = combos.get(hashmapKey);
-                String player2Result = "";
-
-                if(hashmapResult.equals("win")){
-                    player2Result = "lose";
-                    player1.setPoints(player1.getPoints() +1);
-                }else{
-                    player2Result = "win";
-                    player2.setPoints(player2.getPoints() +1);
-                }
-
-                //set player class attributes and print results
-                player1.setMove(answer1);
-                player1.setwOrL(hashmapResult);
-
-
-                player2.setMove(answer2);
-                player2.setwOrL(player2Result);
-                */
                 traslateToHashandResults(answer1, answer2, combos, player1, player2);
 
                 System.out.println("player2 picks: " + answer2);
@@ -278,30 +240,21 @@ public class Main {
                 String winOrLose = player1.getwOrL();
                 System.out.println("you " + winOrLose +"!");
 
-
-
-
                 addToHistory(history, player1, player2);
             }catch(Exception error){
                 error.printStackTrace();
             }
-
-
         }
         //print instructions and loop back to main while loop
         printIntro();
-
-
 
     }
 
     //print game history from arrayList
     //
     private static void printHistory(ArrayList<String> history){
-
+        System.out.println("\n");
         System.out.println("=== GAME HISTORY ===");
-        //System.out.println("WIN: Player-rock computer-scissors");
-        //System.out.println("LOSS: Player-paper computer-scissors");
 
         for(String record: history) {
             System.out.println(record);
@@ -315,7 +268,11 @@ public class Main {
     //
     private static ArrayList<String> addToHistory(ArrayList<String> history, Player player1, Player player2){
 
-        history.add(player1.getwOrL() + " " + "Player-" + player1.getMove() + "computer-" + player2.getMove());
+        if(!player2.isHuman()) {
+            history.add(player1.getwOrL() + " " + "Player-" + player1.getMove() + " computer-" + player2.getMove());
+        }else{
+            history.add(player1.getwOrL() + " " + "Player-" + player1.getMove() + " player2-" + player2.getMove());
+        }
         return history;
 
     }
@@ -323,6 +280,7 @@ public class Main {
     //print to ask for move
     //
     private static void printAskForMove(){
+        System.out.println("\n");
         System.out.println("Type in 'rock' 'paper' or 'scissors' to play.");
         System.out.println("Type 'quit' to go back to the Main Menu");
     }
@@ -339,6 +297,8 @@ public class Main {
         return answer2;
     }
 
+    //take moves and get hashmap result
+    //then update player values accordingly
     private static void traslateToHashandResults(String answer1, String answer2, HashMap<String,String> combos, Player player1, Player player2){
         //translate moves to hashmap result
         String hashmapKey = answer1 + " " +  answer2;
